ee3035d971a8e8d64f0270b205629d24e18d9a6e
spenceraugmntr <71412412+spenceraugmntr@users.noreply.github.com>
2020-09-29 15:35:48 -0700

    Accurate Circular MTF Calculator
    Added finishing touches/made code look nicer. These calculations and graphs are accurate for a circular MTF calculation, but do not match with a Cassegrain MTF calculation.

--- MTF.py
+++ MTF.py
@@ -6,34 +6,35 @@ import numpy as np
 focal_length = float(input("What is your focal length in millimeters?"))
 aperture_diameter = float(input("What is your aperture diameter in millimeters?"))
 central_wavelength = float(input("What is your central wavelength in nanometers?"))
-lpmm = float(input("What is your sensor limiting resolution in lp/mm?"))
+lpmm = float(input("What spatial resolution are you wishing to find a value for (lp/mm)?"))
 
-#converting variable to meters
+#converting variable to mm
 central_wavelength = central_wavelength*1e-6
 
-
-#equations
+#equations/outputs
 cutoff = 1/(central_wavelength*(focal_length/aperture_diameter))
 phi = math.acos(central_wavelength*lpmm*(focal_length/aperture_diameter))
 MTF = (2/math.pi)*(phi-(math.cos(phi)*math.sin(phi)))
 
-print('your cutoff frequency is', cutoff, 'lp/mm')
+print('your cut-off frequency is', cutoff, 'lp/mm')
 print('your phi is', phi, 'lp/mm')
 print('Your MTF is', MTF,'lp/mm')            
                  
 # setting the x - coordinates 
-x = np.linspace(0,500,500)
+x = np.linspace(0,cutoff,500)
 # setting the corresponding y - coordinates 
-phi1 = np.arccos(central_wavelength*x*(focal_length/aperture_diameter))
-
-y = (2/np.pi)*(phi1-(np.cos(phi1)*np.sin(phi1))) 
+y = (2/np.pi)*(np.arccos(x/cutoff)-(np.cos(np.arccos(x/cutoff))*np.sin(np.arccos(x/cutoff)))) 
   
-# potting the points 
+# plotting the points 
 plt.plot(x, y) 
+
+#axis titles
+plt.title('MTF (lp/mm)')
+plt.xlabel('Spatial Resolution (lp/mm)')
+plt.ylabel('MTF')
   
 # function to show the plot 
 plt.show() 
-
 
 
 
