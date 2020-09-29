d5444f25df716f93788720ad4babf769b59a0202
spenceraugmntr <71412412+spenceraugmntr@users.noreply.github.com>
2020-09-29 15:00:35 -0700

    Changed equations/graph
    After changing the equations to output the correct units along with cutoff-frequency, the MTF graph was able to be accurately developed along with correct MTF(lp/mm), Phi(lp/mm), and Cutoff frequency (lp/mm)

--- MTF.py
+++ MTF.py
@@ -9,22 +9,24 @@ central_wavelength = float(input("What is your central wavelength in nanometers?
 lpmm = float(input("What is your sensor limiting resolution in lp/mm?"))
 
 #converting variable to meters
-focal_length = focal_length*.001
-aperture_diameter = aperture_diameter*.001
-central_wavelength = central_wavelength*1e-9
+central_wavelength = central_wavelength*1e-6
 
 
 #equations
+cutoff = 1/(central_wavelength*(focal_length/aperture_diameter))
 phi = math.acos(central_wavelength*lpmm*(focal_length/aperture_diameter))
 MTF = (2/math.pi)*(phi-(math.cos(phi)*math.sin(phi)))
 
-print('your phi is', phi, 'cyc/mrad')
+print('your cutoff frequency is', cutoff, 'lp/mm')
+print('your phi is', phi, 'lp/mm')
 print('Your MTF is', MTF,'lp/mm')            
                  
 # setting the x - coordinates 
-x = np.array([lpmm,500,500] ) 
+x = np.linspace(0,500,500)
 # setting the corresponding y - coordinates 
-y = np.array([MTF,0,1]) 
+phi1 = np.arccos(central_wavelength*x*(focal_length/aperture_diameter))
+
+y = (2/np.pi)*(phi1-(np.cos(phi1)*np.sin(phi1))) 
   
 # potting the points 
 plt.plot(x, y) 
